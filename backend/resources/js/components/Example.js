9ca9680617710c262260ca7c8f87b464a3f36234
Ryotaro Shimizu <ryotaro_625@icloud.com>
2021-10-12 01:15:18 +0900

    20211012 1

---
+++ backend/resources/js/components/Example.js
@@ -0,0 +1,29 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from '@material-ui/core'; //Buttonをインポート
+
+function Example() {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-header">React導入できたわな</div>
+
+                        <div className="card-body">Im an example component!</div>
+                        
+                        <Button color="secondary" variant="contained">ワイがMaterial-UIのボタンやな？？</Button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Example;
+
+if (document.getElementById('app')) {
+    ReactDOM.render(<Example />, document.getElementById('app'));
+}
+
